24bde05ecb6acb0ec806a70c061dc5c72a618b66
“satelliteps” <sp2163675@gmail.com>
2021-12-03 12:50:33 +0545

    files haru inserted

---
+++ hi.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Thankyou for being samekind human being";
+  }
+  
+  function myFunctionn() {
+    document.getElementById("hi").style.textTransform = "capitalize";
+  }
+  
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Sudip Poudel</title>
+    <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+<div class="a">
+  <img class="topcloud" src="cloud.png" alt="cloud">
+  <h1>I'm Sudip</h1>
+  <p class="subhead">I'm new to all of this hope this works fine.</p>
+  <img class="bottomcloud"src="cloud.png" alt="cloud">
+  <img class="mountain"src="mountain.png" alt="mountain">
+</div>
+<div class="middle-container">
+      <div class="profile">
+        <img src="cc.png" alt="pp">
+        <h2>Hello.</h2>
+          <p class="bio">Apart from this to discover new place and to taste different foods is my great hobby.I ❤ working out and tasting different foods.If you have same kind of hobbies then click below.</p>
+  <button onclick="myFunction()">Click me</button>
+    <p id="demo"></p>
+
+    <p id="hi">Lets be more bold.</p>
+
+<button type="button" onclick="myFunctionn()">Transform text</button>
+  </div>
+</div>
+    <div class="bottom-container">
+      <a href="https://www.linkedin.com/">LinkedIn</a>
+      <a href="https://twitter.com/">Twitter</a>
+      <a href="https://www.appbrewery.co/">Website</a>
+<p class="copyright">© 2020 Sudip Sharma Poudel.</p>
+</div>
+<script src="hii.js" charset="utf-8"></script>
+   </body>
+    </html>
